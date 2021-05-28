52ed7a977f718730fe90b0f610b3dd38ce8f00d6
C0d3Math <83771803+C0d3Math@users.noreply.github.com>
2021-05-28 10:24:57 -0400

    Here is why I changed README.md.

---
+++ password_cre8r2.py
@@ -0,0 +1,9 @@
+#By C0d3Math
+#Made Friday 5/28/21
+import random as chooser
+words = open('/etc/dictionaries-common/words').readlines()
+newwords = [word for word in words if not "/'" in word]
+new_password = ""
+for i in range(0,3):
+    new_password = new_password.strip() + chooser.choice(newwords).strip()
+print("Your new password is " + str(new_password) + ". Whenever you see an apostrophe (') and then the letter s in that, ignore that part. That is if you have any of those combinations in it, anyway.")
