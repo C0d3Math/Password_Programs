b9aed0f8fca87b21445f57f08614bd7c2e5c8c76
C0d3Math <83771803+C0d3Math@users.noreply.github.com>
2021-05-28 10:23:43 -0400

    Delete password_cre8r2.py

--- password_cre8r2.py
+++
@@ -1,8 +0,0 @@
-#By C0d3Math
-#Made Friday 5/28/21
-import random as chooser
-words = open('/etc/dictionaries-common/words').readlines()
-new_password = ""
-for i in range(0,3):
-    new_password = new_password.strip() + chooser.choice(words).strip()
-print("Your new password is " + str(new_password) + ". Whenever you see an apostrophe (') and then the letter s in that, ignore that part. That is if you have any of those combinations in it, anyway.")
